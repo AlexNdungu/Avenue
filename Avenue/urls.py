f6e23f70e0c82db90550ba8d2a5a00896f860602
Alex Meta <alexmeta517@gmail.com>
2021-04-08 11:30:39 +0300

    Trial Sitemaps

--- Avenue/urls.py
+++ Avenue/urls.py
@@ -18,10 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
-from avenueShop.sitemaps import MoviesSitemap
+from avenueShop.sitemaps import MoviesSitemap,StaticSitemap
 
 sitemaps = {
     'items':MoviesSitemap,
+    'static':StaticSitemap
 }
 
 urlpatterns = [
--- avenueShop/sitemaps.py
+++ avenueShop/sitemaps.py
@@ -1,6 +1,18 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Movies
+from django.urls import reverse
 
 class MoviesSitemap(Sitemap):
     def items(self):
         return Movies.objects.all()
+
+class StaticSitemap(Sitemap):
+    changefreq = "yearly"
+    priority = 0.8
+    protocol = 'https'
+
+    def items(self):
+        return ['avenueShop:index',]
+
+    def location(self, item):
+            return reverse(item)        
